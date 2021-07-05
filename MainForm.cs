b5090a9414f02f2db7dfaff3eacd63cfbf0d8f8b
DESKTOP-L3KJJ62\ThuongNhu9900 <nguyenthuong100999@gmail.com>
2021-07-06 00:59:45 +0700

    dự án Nhận diện khuôn mặt

---
+++ MainForm.cs
@@ -0,0 +1,224 @@
+﻿
+//Multiple face detection and recognition in real time
+//Using EmguCV cross platform .Net wrapper to the Intel OpenCV image processing library for C#.Net
+//Writed by Sergio Andrés Guitérrez Rojas
+//"Serg3ant" for the delveloper comunity
+// Sergiogut1805@hotmail.com
+//Regards from Bucaramanga-Colombia ;)
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+using System.IO;
+using System.Diagnostics;
+
+namespace MultiFaceRec
+{
+    public partial class FrmPrincipal : Form
+    {
+        //Declararation of all variables, vectors and haarcascades
+        Image<Bgr, Byte> currentFrame;
+        Capture grabber;
+        HaarCascade face;
+        HaarCascade eye;
+        MCvFont font = new MCvFont(FONT.CV_FONT_HERSHEY_TRIPLEX, 0.5d, 0.5d);
+        Image<Gray, byte> result, TrainedFace = null;
+        Image<Gray, byte> gray = null;
+        List<Image<Gray, byte>> trainingImages = new List<Image<Gray, byte>>();
+        List<string> labels= new List<string>();
+        List<string> NamePersons = new List<string>();
+        int ContTrain, NumLabels, t;
+        string name, names = null;
+
+
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+            //Load haarcascades for face detection
+            face = new HaarCascade("haarcascade_frontalface_default.xml");
+            //eye = new HaarCascade("haarcascade_eye.xml");
+            try
+            {
+                //Load of previus trainned faces and labels for each image
+                string Labelsinfo = File.ReadAllText(Application.StartupPath + "/TrainedFaces/TrainedLabels.txt");
+                string[] Labels = Labelsinfo.Split('%');
+                NumLabels = Convert.ToInt16(Labels[0]);
+                ContTrain = NumLabels;
+                string LoadFaces;
+
+                for (int tf = 1; tf < NumLabels+1; tf++)
+                {
+                    LoadFaces = "face" + tf + ".bmp";
+                    trainingImages.Add(new Image<Gray, byte>(Application.StartupPath + "/TrainedFaces/" + LoadFaces));
+                    labels.Add(Labels[tf]);
+                }
+            
+            }
+            catch(Exception e)
+            {
+                //MessageBox.Show(e.ToString());
+                MessageBox.Show("Nothing in binary database, please add at least a face(Simply train the prototype with the Add Face Button).", "Triained faces load", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Initialize the capture device
+            grabber = new Capture();
+            grabber.QueryFrame();
+            //Initialize the FrameGraber event
+            Application.Idle += new EventHandler(FrameGrabber);
+            button1.Enabled = false;
+        }
+
+
+        private void button2_Click(object sender, System.EventArgs e)
+        {
+            try
+            {
+                //Đếm Khuôn Mặt
+                ContTrain = ContTrain + 1;
+
+                //Lấy khung màu xám từ thiết bị chụp
+                gray = grabber.QueryGrayFrame().Resize(320, 240, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+
+                // dò Tìm Khuôn mặt .
+                MCvAvgComp[][] facesDetected = gray.DetectHaarCascade(
+                face,
+                1.2,
+                10,
+                Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING,
+                new Size(20, 20));
+
+                //Phát hiện hành động từ camera
+                foreach (MCvAvgComp f in facesDetected[0])
+                {
+                    TrainedFace = currentFrame.Copy(f.rect).Convert<Gray, byte>();
+                    break;
+                }
+
+                //thay đổi kích thước hình ảnh phát hiện khuôn mặt để có lực so sánh cùng kích thước với
+               //Kiểm tra hình ảnh dùng Emgu
+                TrainedFace = result.Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+                trainingImages.Add(TrainedFace);
+                labels.Add(textBox1.Text);
+
+                //Hiển thị khuôn mặt được thêm trong Khung màu xám
+                imageBox1.Image = TrainedFace;
+
+                //Viết số mặt được đánh dấu vào văn bản tệp để tải thêm
+                File.WriteAllText(Application.StartupPath + "/TrainedFaces/TrainedLabels.txt", trainingImages.ToArray().Length.ToString() + "%");
+
+                //Viết nhãn của các khuôn mặt được đánh dấu trong một văn bản tệp để tải thêm
+                for (int i = 1; i < trainingImages.ToArray().Length + 1; i++)
+                {
+                    trainingImages.ToArray()[i - 1].Save(Application.StartupPath + "/TrainedFaces/face" + i + ".bmp");
+                    File.AppendAllText(Application.StartupPath + "/TrainedFaces/TrainedLabels.txt", labels.ToArray()[i - 1] + "%");
+                }
+
+                MessageBox.Show(textBox1.Text + "Đã thêm khuôn mặt của bạn", "Thêm Khuôn Mặt Thành Công", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch
+            {
+                MessageBox.Show("Không Nhận Diện Được Khuôn Mặt", "Thông Báo Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+
+        void FrameGrabber(object sender, EventArgs e)
+        {
+            label3.Text = "0";
+           
+            NamePersons.Add("");
+
+
+            //Nhận thiết bị chụp hình dạng khung hiện tại
+            currentFrame = grabber.QueryFrame().Resize(320, 240, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+
+            //Chuyển nó sang khung màu Xám
+            gray = currentFrame.Convert<Gray, Byte>();
+
+                    //Dò Khuôn mặt
+                    MCvAvgComp[][] facesDetected = gray.DetectHaarCascade(
+                  face,
+                  1.2,
+                  10,
+                  Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING,
+                  new Size(20, 20));
+
+                    //Hành động cho từng phần tử được phát hiện
+                    foreach (MCvAvgComp f in facesDetected[0])
+                    {
+                        t = t + 1;
+                        result = currentFrame.Copy(f.rect).Convert<Gray, byte>().Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+                        //vẽ khuôn mặt được phát hiện trong kênh thứ 0 (xám) với màu xanh lam
+                        currentFrame.Draw(f.rect, new Bgr(Color.Red), 2);
+
+
+                        if (trainingImages.ToArray().Length != 0)
+                        {
+                            //Nhận dạng khuôn mặt với số lượng hình ảnh được thêm vào.
+                        MCvTermCriteria termCrit = new MCvTermCriteria(ContTrain, 0.001);
+
+                           //Eigen nhận dạng khuôn mặt
+                           EigenObjectRecognizer recognizer = new EigenObjectRecognizer(
+                           trainingImages.ToArray(),
+                           labels.ToArray(),
+                           3000,
+                           ref termCrit);
+
+                        name = recognizer.Recognize(result);
+
+                        //Vẽ nhãn cho từng khuôn mặt được phát hiện và nhận dạng
+                        currentFrame.Draw(name, ref font, new Point(f.rect.X - 2, f.rect.Y - 2), new Bgr(Color.LightGreen));
+
+                        }
+
+                            NamePersons[t-1] = name;
+                            NamePersons.Add("");
+
+
+                        //Đặt số lượng khuôn mặt được phát hiện trên hiện trường
+                        label3.Text = facesDetected[0].Length.ToString();
+                       
+                      
+
+                    }
+                        t = 0;
+
+                        //Tên ghép của những người được công nhận
+                       for (int nnn = 0; nnn < facesDetected[0].Length; nnn++)
+                        {
+                            names = names + NamePersons[nnn] + ", ";
+                        }
+                    //Hiển thị các khuôn mặt được xử lý và nhận dạng
+                    imageBoxFrameGrabber.Image = currentFrame;
+                    label4.Text = names;
+                    names = "";
+                    //Xóa danh sách tên
+                    NamePersons.Clear();
+
+                }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Process.Start("");
+        }
+
+    }
+}
